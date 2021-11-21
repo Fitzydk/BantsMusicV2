e3da40a2cfba5372f778bb64d084f6fdf0764c93
Brogan Fitzgerald <broganfitzy@gmail.com>
2021-11-21 03:02:36 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const app = require('express')();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Server is up.'));
 
 module.exports = () => {
-  app.listen(3000);
+  app.listen(PORT, function() {
+    console.log(`Listening on Port ${PORT}`);
+  });
 }
